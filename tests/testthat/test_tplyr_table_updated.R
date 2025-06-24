d8ae4708d04a5b2f95204203c6fbaf5fb1800a2e
Matthias,Korbinian (MED BDS) BIP-DE-B <korbinian.matthias@boehringer-ingelheim.com>
2025-06-24 08:22:41 +0100

    Update tests

--- tests/testthat/test_tplyr_table_updated.R
+++ tests/testthat/test_tplyr_table_updated.R
@@ -12,11 +12,8 @@ local({
 
   app <- shinytest2::AppDriver$new(root_app$get_url())
 
-  # app <- shinytest2::AppDriver$new("C:/Users/matthiak/OneDrive - Boehringer Ingelheim/Documents/Davinci/GitHub/dv.tables/tests/testthat/app/tplyr_table/app.R")
-
-
   testthat::test_that("Table cell click generates corresponding listing", {
-    
+
     app$set_inputs(
       `test-row_id` = list(index = 8),
       `test-col_id` = list(column = "var1_Xanomeline Low Dose"),
@@ -25,21 +22,21 @@ local({
 
     actual <- shiny::isolate(app$get_value(export = "test-subject_subset")())
     actual <- as.character(actual)
-    
-    expeced <- c(
-      "01-701-1097", "01-701-1192", "01-701-1317", "01-701-1324", "01-701-1442", 
-      "01-703-1379", "01-704-1135", "01-704-1218", "01-705-1292", "01-705-1431", 
-      "01-708-1084", "01-708-1348", "01-709-1020", "01-709-1326", "01-710-1235", 
-      "01-713-1043", "01-713-1073", "01-714-1195", "01-715-1085", "01-716-1103", 
+
+    expected <- c(
+      "01-701-1097", "01-701-1192", "01-701-1317", "01-701-1324", "01-701-1442",
+      "01-703-1379", "01-704-1135", "01-704-1218", "01-705-1292", "01-705-1431",
+      "01-708-1084", "01-708-1348", "01-709-1020", "01-709-1326", "01-710-1235",
+      "01-713-1043", "01-713-1073", "01-714-1195", "01-715-1085", "01-716-1103",
       "01-716-1157", "01-716-1167", "01-717-1004", "01-717-1446", "01-718-1254"
       )
-    
+
     testthat::expect_equal(actual, expected)
-    
+
   })
 
   testthat::test_that("output can be switched", {
-    
+
     table1 <- app$get_value(output = "test-table_output")
     app$set_inputs(`test-sel_output` = "Tabel 2")
     table2 <- app$get_value(output = "test-table_output")
@@ -51,7 +48,7 @@ local({
     table_output <- app$get_value(output = "test-table_output")
     expected <- structure("{\"x\":null,\"evals\":[],\"jsHooks\":null,\"deps\":[]}", class = "json")
     expect_equal(table_output, expected)
-    
+
     listings_data <- app$get_value(export = "test-listings_data")
     expect_equal(names(listings_data), c( "adsl", "adae"))
     # display of data will be tested in dv.listings
@@ -63,15 +60,16 @@ local({
     default_table_data <- parsed_table_output$x$tag$attribs$data
     # remove placebo from data
     default_table_data_filtered <- default_table_data[!names(default_table_data) %in% "var1_Placebo"]
-    
+
+    app$set_inputs(`global_filter-vars` = "ARM")
     app$set_inputs(`global_filter-ARM` = c("Xanomeline High Dose", "Xanomeline Low Dose", "Screen Failure"))
-    
+
     table_output_filtered <- app$get_value(output = "test-table_output")
     parsed_table_output_filtered <- jsonlite::fromJSON(table_output_filtered)
     filtered_table_data <- parsed_table_output_filtered$x$tag$attribs$data
-    
+
     expect_equal(filtered_table_data, default_table_data_filtered)
-    
+
   })
 })
 
