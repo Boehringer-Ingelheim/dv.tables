daf6257658b72ff17c8e04fd3b6629a0116583c0
Shah <shahyash@inter-standard-dy-cpr-interactive-st-18.workbenchhpc5795f00e20251024034615.pcluster>
2026-01-29 10:24:22 +0000

    - fix test; runs locally

--- tests/testthat/test-tplyr_tables.R
+++ tests/testthat/test-tplyr_tables.R
@@ -196,23 +196,11 @@ local({
   testthat::test_that("tabbed output can be switched" |>
     vdoc[["add_spec"]](specs$Tplyr_tables$output_switching), {
 
-    # Wait until the tabsetPanel element exists AND has input binding
-    app$wait_for_js(
-      "(() => {
-         const el = document.getElementById('test-title_tabs');
-         return !!el && el.classList.contains('shiny-bound-input');
-       })()",
-      timeout = 10000
-    )
+    table1 <- app$get_value(output = "mock_tplyr_tabs-table_output")
+    app$set_inputs(`mock_tplyr_tabs-title_tabs` = "Tabel 2")
+    table2 <- app$get_value(output = "mock_tplyr_tabs-table_output")
 
-
-    table1 <- app$get_value(output = "test-table_output")
-
-    app$set_inputs(`test-title_tabs` = "Tabel 2")
-    app$wait_for_value(input = "test-title_tabs", value = "Tabel 2")
-    app$wait_for_idle()
-
-    table2 <- app$get_value(output = "test-table_output")
     testthat::expect_false(table1 == table2)
+
   })
 })
